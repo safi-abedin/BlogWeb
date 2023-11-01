aacffade33480cb776d2eb2a2033cf5c1e472576
safi-abedin <kadersafi@2050>
2023-11-01 21:10:28 +0600

    Completed Model binding to Save Data in DataBase .

--- Blog/Blog.Web/Controllers/AdminTagsController.cs
+++ Blog/Blog.Web/Controllers/AdminTagsController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Web.Data;
+using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private AplicationDbContext _aplicationDbContext { get; }
+        public AdminTagsController(AplicationDbContext aplicationDbContext) 
+        {
+            _aplicationDbContext = aplicationDbContext;
+        }
+
+        
+
         [HttpGet]
         public IActionResult Add()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            //Mapping AddTagRequest to tag model
+            var data = new Tag
+            {
+
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+            _aplicationDbContext.Tags.Add(data);
             return View();
         }
     }
