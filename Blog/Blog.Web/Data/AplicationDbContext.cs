608d61dfe1f4e7ea3bf0c5322b2305861ee4b96b
safi-abedin <kadersafi@2050>
2023-11-01 19:13:53 +0600

    Created Service to Connect With SqlServer .

---
+++ Blog/Blog.Web/Data/AplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Data
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
