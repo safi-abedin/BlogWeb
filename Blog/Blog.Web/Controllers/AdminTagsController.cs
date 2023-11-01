be3f62e1f8ea0c650c469f3f775a31dba76a9aaa
safi-abedin <kadersafi@2050>
2023-11-01 20:02:55 +0600

    Created a controller for Admin tag Create

---
+++ Blog/Blog.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
